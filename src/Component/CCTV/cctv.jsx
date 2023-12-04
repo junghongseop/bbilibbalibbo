117a84adb755d6f77b3ddf692de8fa47b6be5845
hongseop <a01082372487@gmail.com>
2023-12-04 23:53:58 +0900

    ADD :: vector 이미지

--- src/Component/CCTV/cctv.jsx
+++ src/Component/CCTV/cctv.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import img from "../../img/photo.svg";
 import * as S from "./style";
+import vector from "../../img/vector.svg";
+import { Link } from "react-router-dom";
 
 const CCTV = () => {
   return (
     <body>
       <S.CctvImage src={img} alt="CCTV" />
+      <S.VectorContainer>
+        <S.VectorText>
+          <Link to="/cctv">
+            <S.VectorImage src={vector} alt="vector" />
+          </Link>
+        </S.VectorText>
+      </S.VectorContainer>
     </body>
   );
 };
--- src/Component/CCTV/style.js
+++ src/Component/CCTV/style.js
@@ -9,3 +9,31 @@ export const CctvImage = style.img`
   position: relative;
   top: 89px;
 `;
+
+export const VectorContainer = style.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const VectorText = style.span`
+  color: #000;
+  font-family: Inter;
+  font-size: 25px;
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+  margin-left: 5px;
+
+  position: fixed;
+  left: 120px;
+  `;
+
+export const VectorImage = style.img`
+  width: auto;
+  height: auto;
+  flex-shrink: 1;
+
+  position: fixed;
+  left: 340px;
+  top: 600px;
+`;
