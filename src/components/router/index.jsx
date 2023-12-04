63ed17c3da93993d77924c150f6c2c7d01a16d0a
hongseop <a01082372487@gmail.com>
2023-12-04 17:16:38 +0900

    DELETE :: 코드 수정

--- src/components/router/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Routes, Route } from "react=router-dom";
-import Main from "../../pages/Mainpage/Mainpage";
-
-const Routers = () => {
-  return <Routes></Routes>;
-};
-
-export default Routers;
