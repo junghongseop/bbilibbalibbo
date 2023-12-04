df031d3d020be51a50c1f7080877aa4adc9612d8
hongseop <a01082372487@gmail.com>
2023-12-04 17:33:41 +0900

    FIX :: 로고 위치 수정

--- src/Component/Mainpage/style.js
+++ src/Component/Mainpage/style.js
@@ -2,9 +2,9 @@ import style from "styled-components";
 
 export const NavbarContainer = style.div`
   width: 98%;
-  height: 80px;
+  height: 90px;
   background: #fff;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 0px 2px 0px rgba(0, 0, 0, 0.9);
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -34,11 +34,12 @@ export const LogoText = style.span`
 `;
 
 export const LogoImage = style.img`
-  width: 48px;
-  height: 55px;
-  flex-shrink: 0;
+  width: 200px;
+  height: 35px;
+  flex-shrink: 1;
   background: #fff;
 
   position: fixed;
-  left: 65px;
+  left: 90px;
+  top: 30px;
 `;
