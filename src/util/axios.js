7a78b87963cc8a98387e80e3fe2eafe503ed1fc7
hongseop <a01082372487@gmail.com>
2023-12-06 09:59:44 +0900

    ADD :: axios설정

---
+++ src/util/axios.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// const currentDomain = window.location.hostname;
+
+export const axiosInstance = axios.create({
+  baseURL: "http://10.129.57.213:3000/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  // withCredentials: true  // 이렇게 설정하면 요청시에 자동으로 쿠키가 포함됩니다.
+});
+
+export default axiosInstance;
