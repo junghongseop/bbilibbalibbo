5885d0a94c69406f5ef834f7fe69c8e14f7c6f1b
hongseop <a01082372487@gmail.com>
2023-12-10 02:21:57 +0900

    FEAT :: 비디오 모달 완성

--- src/Component/CCTVcheck/cctvcheck.jsx
+++ src/Component/CCTVcheck/cctvcheck.jsx
@@ -1,15 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
+import { Link } from "react-router-dom";
+import Modal from "react-modal";
 import Navbar from "../navbar";
-import video1 from "../../video/AnyConv.com__2023-12-07 19_59_49.mp4";
+import video1 from "../../video/2023-12-07 19_59_49.mp4";
+import video2 from "../../video/2023-12-07 20_03_13.mp4";
+import time1 from "../../img/time1.svg";
+import time2 from "../../img/time2.svg";
+import x from "../../img/wx.svg";
 
 function CCTVcheck() {
+  const [isOpenningModalOpen1, setIsOpenningModalOpen1] = useState(false);
+  const [isOpenningModalOpen2, setIsOpenningModalOpen2] = useState(false);
+
   return (
     <S.Body>
       <Navbar />
-      <video width="360" height="280" controls="controls">
-        <source src={video1} type="video/mp4" />
-      </video>
+      <S.H2>영상 조회</S.H2>
+      <S.Open onClick={() => setIsOpenningModalOpen1(true)}>
+        <S.Time1Image src={time1} alt="time1" />
+      </S.Open>
+      <S.Open onClick={() => setIsOpenningModalOpen2(true)}>
+        <S.Time2Image src={time2} alt="time2" />
+      </S.Open>
+
+      <Modal
+        isOpen={isOpenningModalOpen1}
+        onRequestClose={() => setIsOpenningModalOpen1(false)}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.40)",
+          },
+          content: {
+            overflow: "hidden",
+            top: "60%",
+            left: "28%",
+            right: "0px",
+            width: "720px",
+            height: "515px",
+            transform: "translateY(-50%)",
+            background: "none",
+            border: "none",
+          },
+        }}
+      >
+        <S.ModalContent>
+          <S.PopupVideo src={video1} autoPlay controls />
+          <Link to="/cctvcheck" onClick={() => setIsOpenningModalOpen1(false)}>
+            <S.XImage src={x} alt="x" />
+          </Link>
+        </S.ModalContent>
+      </Modal>
+
+      <Modal
+        isOpen={isOpenningModalOpen2}
+        onRequestClose={() => setIsOpenningModalOpen1(false)}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.40)",
+          },
+          content: {
+            overflow: "hidden",
+            top: "60%",
+            left: "28%",
+            right: "0px",
+            width: "720px",
+            height: "515px",
+            transform: "translateY(-50%)",
+            background: "none",
+            border: "none",
+          },
+        }}
+      >
+        <S.ModalContent>
+          <S.PopupVideo src={video2} autoPlay controls />
+          <Link to="/cctvcheck" onClick={() => setIsOpenningModalOpen2(false)}>
+            <S.XImage src={x} alt="x" />
+          </Link>
+        </S.ModalContent>
+      </Modal>
     </S.Body>
   );
 }
--- src/Component/CCTVcheck/style.js
+++ src/Component/CCTVcheck/style.js
@@ -1,7 +1,15 @@
 import styled from "styled-components";
 
-export const H2 = styled.h2`
-  text-align: center;
+export const H2 = styled.div`
+  position: absolute;
+  width: 400px;
+  height: 30px;
+  font-size: 30px;
+  font-weight: 700;
+  line-height: 50px;
+  text-align: left;
+  top: 160px;
+  left: 200px;
 `;
 
 export const Body = styled.body`
@@ -9,4 +17,53 @@ export const Body = styled.body`
   align-items: center;
   justify-content: center;
   padding: 13% 0 0 0;
+`;
+
+export const Time1Image = styled.img`
+  width: 600px;
+  height: auto;
+  position: absolute;
+  top: 230px;
+  left: 200px;
+`;
+
+export const Time2Image = styled.img`
+  width: 600px;
+  height: auto;
+  position: absolute;
+  top: 280px;
+  left: 200px;
+  border: 1px soild;
+`;
+
+export const ModalContent = styled.div`
+  display: flex !important;
+  width: 0px;
+  height: 0px;
+  padding: 0px !important;
+  border-radius: 10px !important;
+  justify-content: center !important;
+  margin: 0 auto !important;
+  overflow: hidden !important; // 추가된 코드
+`;
+
+export const PopupVideo = styled.video`
+  position: absolute;
+  top: 0px;
+  width: 760pxpx;
+  height: 515px;
+  left: 0px;
+`;
+
+export const XImage = styled.img`
+  width: 25px; // 사이즈 조절
+  height: 25px;
+  position: absolute;
+  right: 30px; // 위치 조절
+  top: 5px;
+`;
+
+export const Open = styled.div`
+  display: flex;
+  align-items: center;S
 `;
